d47feb9c8b5650213cee5039c5e13880dd1885ae
indrajit <eendroroy@gmail.com>
2018-04-12 01:49:47 +0600

    update installer

--- zsh-autoenv-templates-installer.zsh
+++ zsh-autoenv-templates-installer.zsh
@@ -2,11 +2,12 @@
 
 function ati() {
 
-	_TEMPLATE_NAMES=(JAVA PYTHON RUBY)
+	SCRIPT_ROOT=${0:A:h}
+	source "${SCRIPT_ROOT}/autoenv-templates/__TEMPLATE_NAMES.zsh"
 
-	if [[ ${_TEMPLATE_NAMES[(i)$1]} > $#_TEMPLATE_NAMES ]]; then
+	if [[ ${__TEMPLATE_NAMES[(i)$1]} > $#__TEMPLATE_NAMES ]]; then
 		(>&2 echo "Template for ${1} not available.")
-		(>&2 echo "Availavle template are ${_TEMPLATE_NAMES}.")
+		(>&2 echo "Availavle template are ${__TEMPLATE_NAMES}.")
 		return 1
 	fi
 
@@ -18,15 +19,7 @@ function ati() {
 	echo "       into '${ATI_ENV_IN}' and '${ATI_ENV_OUT}' ..."
 	echo
 
-	if [[ "${1}" == "JAVA" ]]; then
-		wget https://github.com/eendroroy/autoenv-templates/raw/master/java/env.in -O ${ATI_ENV_IN} -q
-		wget https://github.com/eendroroy/autoenv-templates/raw/master/java/env.out -O ${ATI_ENV_OUT} -q
-	elif [[ "${1}" == "RUBY" ]]; then
-		wget https://github.com/eendroroy/autoenv-templates/raw/master/ruby/env.in -O ${ATI_ENV_IN} -q
-		wget https://github.com/eendroroy/autoenv-templates/raw/master/ruby/env.out -O ${ATI_ENV_OUT} -q
-	elif [[ "${1}" == "PYTHON" ]]; then
-		wget https://github.com/eendroroy/autoenv-templates/raw/master/python/env.in -O ${ATI_ENV_IN} -q
-		wget https://github.com/eendroroy/autoenv-templates/raw/master/python/env.out -O ${ATI_ENV_OUT} -q
-	fi
+	\cp "${SCRIPT_ROOT}/autoenv-templates/${1}/env.in" "${ATI_ENV_IN}"
+	\cp "${SCRIPT_ROOT}/autoenv-templates/${1}/env.out" "${ATI_ENV_OUT}"
 
 }
